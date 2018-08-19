24121957574c736fce2830dd4b354b9fd400c956
replica <undefined@JHui-MacBook-Pro.local>
2018-08-19 17:20:48 +0900

    FrontEnd Sass setting commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import style from '../src/test.scss'
+console.log('test');
+console.log(style)
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className={style.test}>
         {/* <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
